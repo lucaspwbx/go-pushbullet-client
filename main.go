88456f6812dc8a5f995976d3364d0014e3a6a1a7
Lucas Weiblen <lucasweiblen@gmail.com>
2014-09-25 22:10:35 -0300

    bootstrap client, contacts actions working

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 )
 
 func main() {
-	cli := client.NewClient("teste")
+	cli := client.NewClient("token")
 	resp, code, _ := cli.GetContacts()
 	fmt.Println(resp)
 	fmt.Println(code)
+	resp, code, _ = cli.CreateContact(client.Params{"name": "joao", "email": "foo@joao.com"})
+	fmt.Println(resp)
+	fmt.Println(code)
+	resp, code, _ = cli.UpdateContact(client.Params{"iden": "ujDigsFMxWesjAjjvzVOLY", "name": "blindpigs"})
+	fmt.Println(resp)
+	fmt.Println(code)
+	//resp, code, _ = cli.DeleteContact(client.Params{"iden": "ujDigsFMxWesjz0F6E05sa"})
+	//fmt.Println(resp)
+	//fmt.Println(code)
 }
